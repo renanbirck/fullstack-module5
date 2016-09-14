a425106942407e06994e687076ecaf93013007ae
Renan Birck <renan.ee.ufsm@gmail.com>
2016-09-14 13:16:26 -0300

    add minification support.

--- assignment1/app.js
+++ assignment1/app.js
@@ -2,49 +2,37 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController)
+LunchCheckController.$inject = ['$scope']
 
-.controller('LunchCheckController', function ($scope) {
-    // $scope.message = "Angular is up";
 
+function LunchCheckController ($scope) {
     $scope.checkIfTooMuch = function () {
       $scope.message = $scope.getMessageForLunchItems($scope.countLunchItems());
-
     }
-
     $scope.countLunchItems = function() {
-
       var dishes = $scope.dishes;
       console.log(dishes)
       if(dishes === undefined || dishes === null) {
         return 0;
       }
-
       dishes = dishes + ''; // force dishes into a string
-
       if(dishes === "") {
         return 0;
       }
-
       dishes = dishes.split(',');
       console.log(dishes);
       return dishes.length;
-
     }
-
     $scope.getMessageForLunchItems = function(numItems) {
       if(numItems == 0) {
         return "Please enter data first";
-
       } else if (numItems <= 3) {
         return "Enjoy!";
-
       } else {
         return "TOO MUCH!";
       }
     }
-
-
-
-});
+  }
 
 })();
