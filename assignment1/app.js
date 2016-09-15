fe4bcd1f62c5eb49eddb6adf59049629847b158c
Renan Birck <renan.ee.ufsm@gmail.com>
2016-09-15 17:17:44 -0300

    simplify code; breaks when empty box.

--- assignment1/app.js
+++ assignment1/app.js
@@ -6,33 +6,27 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope']
 
 
-function LunchCheckController ($scope) {
-    $scope.checkIfTooMuch = function () {
-      $scope.message = $scope.getMessageForLunchItems($scope.countLunchItems());
-    }
-    $scope.countLunchItems = function() {
-      var dishes = $scope.dishes;
-      console.log(dishes)
-      if(dishes === undefined || dishes === null) {
-        return 0;
-      }
-      dishes = dishes + ''; // force dishes into a string
-      if(dishes === "") {
-        return 0;
-      }
-      dishes = dishes.split(',');
-      console.log(dishes);
-      return dishes.length;
-    }
-    $scope.getMessageForLunchItems = function(numItems) {
-      if(numItems == 0) {
-        return "Please enter data first";
-      } else if (numItems <= 3) {
-        return "Enjoy!";
-      } else {
-        return "TOO MUCH!";
-      }
-    }
+    function LunchCheckController ($scope) {
+        $scope.dishes = ""
+
+        $scope.checkIfTooMuch = function () {
+          $scope.message = $scope.getMessageForLunchItems($scope.countLunchItems());
+        }
+        $scope.countLunchItems = function() {
+          var dishes = $scope.dishes;
+          dishes = dishes.split(',');
+          console.log(dishes);
+          return dishes.length;
+        }
+        $scope.getMessageForLunchItems = function(numItems) {
+          if(numItems == 0) {
+            return "Please enter data first";
+          } else if (numItems <= 3) {
+            return "Enjoy!";
+          } else 
+            return "TOO MUCH!";
+            
+        }
   }
 
 })();
